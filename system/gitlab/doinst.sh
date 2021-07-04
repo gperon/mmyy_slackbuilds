9710c4f863f12539e77e9b678470b4a9acec18a1
Giorgio Peron <giorgio_peron@libero.it>
2021-07-04 20:10:33 +0200

    20210704.1 gperon global branch merge

---
+++ system/gitlab/doinst.sh
@@ -0,0 +1,24 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+# Keep same perms on :
+preserve_perms etc/gitlab/gitlab.rb.new
+config etc/rc.d/rc.gitlab.new

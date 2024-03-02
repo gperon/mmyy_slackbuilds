696abf67c6ff810b32d3bdb4112907bebef0d176
Giorgio Peron <giorgio.peron@gmail.com>
2024-03-02 12:46:46 +0100

    20240302.1 gperon global branch merge

---
+++ development/cudatoolkit12/doinst.sh
@@ -0,0 +1,5 @@
+# Define compilers for CUDA to use.
+# This allows us to use older versions of GCC if we have to.
+ln -sf usr/bin/gcc usr/share/cuda/bin/gcc 
+ln -sf usr/bin/g++ usr/share/cuda/bin/g++
+ldconfig
